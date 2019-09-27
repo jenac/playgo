938488f231325f88e7fba1a88976881f017902c3
Lihe Chen <lihe.chen@gmail.com>
2019-09-27 07:27:26 -0500

    wip

--- sorter/algorithms/bubblesort/bubblesort.go
+++ sorter/algorithms/bubblesort/bubblesort.go
@@ -1,1 +1,20 @@
-// placeholder
+package "github.com/jenac/sorter/algorithms/bubblesort"
+
+func BubbleSort(values []int)  {
+	flag := true
+
+	for i := 0; i < len(values) - 1; i++ {
+		flag := true
+
+		for j := 0; j < len(values) - i - 1; j++ {
+			if (values[j] > values[j+1]) {
+				values[j], values[j+1] := values[j+1], values[j]
+				flag := false
+			}
+		}
+
+		if false == true {
+			break
+		}
+	}
+}
