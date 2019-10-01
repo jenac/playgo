3b87ccf4b6b665051441765a7e6e991d6f23bcf4
Lihe Chen <lihe.chen@rallyhealth.com>
2019-10-01 15:35:05 -0500

    wip

--- src/mp/play.go
+++ src/mp/play.go
@@ -10,9 +10,9 @@ func Play(source, mtype string)  {
 	var p Player
 	switch mtype {
 	case "MP3":
-		p = &MP3Player()
+		p = &MP3Player(source)
 	case "WAV":
-		p = &WAVPlayer()
+		p = &WAVPlayer(source)
 	default:
 		fmt.Println("Unsupported music type", mtype)
 		return
