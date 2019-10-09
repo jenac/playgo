b8eff04c87bddee68421eb2da2cc4ff383d00bf0
Lihe Chen <lihe.chen@gmail.com>
2019-10-08 22:57:17 -0500

    wip

---
+++ src/cg/center.go
@@ -0,0 +1,62 @@
+package cg
+
+import {
+	"encoding/json"
+	"errors"
+	"sync"
+	"ipc"
+}
+
+var _ ipc.Server = &CenterServer
+
+type Message struct {
+	From string "from"
+	To string "to"
+	Content string "content"
+}
+
+type CenterServer struct {
+	servers map[string] ipc.Server
+	players []*Player
+	rooms []*Room
+	mutex sync.RWMutex
+}
+
+func NewCenterServer() *CenterServer {
+	servers := make(map[string] ipc.Server)
+	players := make([]*Player, 0)
+
+	return &CenterServer { servers:servers, players:players}
+}
+
+func (server *CenterServer) addPlayer(params string) error {
+	player := NewPlayer()
+
+	err := json.Unmarshal([]byte(params), &player)
+	if err != nil {
+		return err
+	}
+
+	server.mutex.Lock()
+	defer server.mutex.Unlock()
+
+	// should check if already added
+	server.players = append(server.players, player)
+
+	return nil
+}
+
+func (server *CenterServer)removePlayer(params string) error {
+	server.mutex.Lock()
+	defer server.mutex.Unlock()
+
+	for i, v := range server.players {
+		if v.Name == params {
+			if len(server.players) == 1 {
+				server.players = make([]*player, 0)
+			} else if i == len(server.players) -1 {
+				server.players = server.players[:i-1]
+			} else if i == 0
+		}
+	}
+}
