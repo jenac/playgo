6221ef7a70dbf2afd925fa65fc1c7b7b0976419b
Lihe Chen <lihe.chen@gmail.com>
2019-10-09 20:29:56 -0500

    wip

--- src/cg/center.go
+++ src/cg/center.go
@@ -56,7 +56,57 @@ func (server *CenterServer)removePlayer(params string) error {
 				server.players = make([]*player, 0)
 			} else if i == len(server.players) -1 {
 				server.players = server.players[:i-1]
-			} else if i == 0
+			} else if i == 0 {
+				server.players = server.players[1:]
+			} else {
+				server.players = append(server.players[:i-1], server.players[:i+1]...)
+			}
+			return nil
 		}
+	}
+	return errors.New("Player not found")
+}
+
+func (server *CenterServer)listPlayer(params string)(players string, err error) {
+	server.mutex.RLock()
+	defer server.mutex.RUnlock()
+
+	if len(server.players) > 0 {
+		b, _ := json.Marshal(server.players)
+		players = string(b)
+	} else {
+		err = errors.New("No player online.")
+	}
+	return
+}
+
+func (server *CenterServer)broadcast(params string) error {
+	var message Message
+	err := json.Unmarshal([]byte(params), &message)
+	if err != nil {
+		return err
+	}
+
+	server.mutex.Lock()
+	defer server.mutex.Unlock()
+
+	if len(server.players) > 0 {
+		for _, player := range server.players {
+			player.mq <- &message
+		}
+	} else {
+		err = errors.New("No player online.")
+	}
+	return err
+}
+
+func (server *CenterServer)Handle(method, params string) *ipc.Response {
+	switch method {
+		case "addPlayer":
+			err := server.addPlayer(params)
+			if err := nil {
+				return &ipc.Response{ Code: err.Error() }
+			}
+		case 
 	}
 }
