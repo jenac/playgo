036bfec4c27b693d9516ff7d22c2a86341efaa3f
Lihe Chen <lihe.chen@rallyhealth.com>
2019-10-23 09:25:35 -0500

    wip

---
+++ src/photoweb/photoweb.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+function uploadHandler(w http.ResposeWriter, r *http.Request) {
+	if r.Method == "GET" {
+		io.WriteString(w, "<form method=\"POST\" action=\"/upload\" "+
+		" enctype=\"multipart/form-data\">"+
+		"Choose an image to upload: <input name=\"image\" type=\"file\" />"+
+		"<input type=\"submit\" value=\"Upload\" />"+
+		"</form>")
+		return
+	}
+}
+
+func main() {
+	http.HandleFunc("/upload", uploadHandler)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err.Error())
+	}
+}
